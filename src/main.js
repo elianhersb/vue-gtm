b7843f820e6a64a51ad878408b57ee69f74c49f6
Elianhers Blanco <elianhers.b@gmail.com>
2023-03-15 08:24:31 -0400

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGtm from '@gtm-support/vue2-gtm';
 
 Vue.config.productionTip = false
+
+Vue.use(VueGtm, {
+  id: "GTM-T9DQ9K8", 
+  defer: false, // defaults to false. Script can be set to `defer` to increase page-load-time at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible)
+  enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
+  debug: true, // Whether or not display console logs debugs (optional)
+  loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
+  ignoredViews: ["homepage"], // Don't trigger events for specified router names (case insensitive) (optional)
+  trackOnNextTick: false, // Whether or not call trackView in Vue.nextTick
+});
 
 new Vue({
   render: h => h(App),

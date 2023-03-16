c3c4c0fd12f873b39bafc409fc93aa835ec24d3c
Elianhers Blanco <elianhers.b@gmail.com>
2023-03-15 21:19:00 -0400

    gtm-03 test-03

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,10 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button
+      @click="onSetGTM()"
+    >
+      send event
+    </button>
   </div>
 </template>
 
@@ -37,15 +15,20 @@ export default {
     msg: String
   },
   mounted(){
-    console.log('gtm')
-    this.$gtm.trackEvent({
-      event: null, // Event type [default = 'interaction'] (Optional)
-      category: "PRUEBA",
-      action: "click",
-      label: "pruebaVue",
-      value: 'vuegtm',
-      noninteraction: false, // Optional
-    });
+    console.log('gtm01')
+    this.$gtm.trackView("MyScreenName", "currentPath");
+  },
+  methods:{
+    onSetGTM(){
+      this.$gtm.trackEvent({
+        event: null, // Event type [default = 'interaction'] (Optional)
+        category: "onSetGTM",
+        action: "click",
+        label: "SetGTM",
+        value: 'vuegtm02',
+        noninteraction: false, // Optional
+      });
+    }
   }
 }
 </script>
